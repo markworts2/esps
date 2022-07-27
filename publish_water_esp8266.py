d95cc54136af0ce47cf3c0ec610886e77f9535db
markworts2 <mark.worts@gmail.com>
2022-07-27 14:54:16 +0100

    fix water Pin

--- publish_water_esp8266.py
+++ publish_water_esp8266.py
@@ -28,7 +28,7 @@ SERVER = '192.168.86.248'  # MQTT Server Address (Change to the IP address of yo
 CLIENT_ID = 'ESP32_DHT22_Sensor'
 TOPIC = b'temp_humidity'
 rtc = machine.RTC()
-adc = ADC(0)  #get the ADC0 pin reading (Analogue to digital pin)
+adc = ADC(Pin(0))  #get the ADC0 pin reading (Analogue to digital pin)
 
 date_str = "{2:02d}/{1:02d}/{0:4d} {4:02d}:{5:02d}".format(*rtc.datetime())
 
