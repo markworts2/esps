3959fdba0c868a2b3f80db7c556b9984ed33fcd3
markworts2 <mark.worts@gmail.com>
2023-05-05 16:30:49 +0100

    display set 4 12 1

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -12,7 +12,7 @@ print('version 20230505 v_0.02')
 
 
 try:
-    display = max7219.SevenSegment(digits=16, scan_digits=8, cs=12, spi_bus=2, reverse=True)
+    display = max7219.SevenSegment(digits=8, scan_digits=4, cs=12, spi_bus=1, reverse=True)
     display.text("ABCDEF")
     display.number(3.14159)
     display.message("Hello World")
