1059d9ca99aceeeb4470387aee44f19893db1e7b
markworts2 <mark.worts@gmail.com>
2023-05-05 15:42:52 +0100

    numeric only

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -10,7 +10,7 @@ print('version 20230505 v_0.01')
 
 spi = SPI(1, baudrate=10000000)
 screen = max7219.Max7219(64, 8, spi, Pin(12))
-screen.text('ABCDEFGH', 0, 0, 1)
+screen.text('12345678', 0, 0, 1)
 screen.show()
 
 sleep(100000)
