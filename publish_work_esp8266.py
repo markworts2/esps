f2cfcb9535eeff7e2abb0c13e5ae80cf881d42ae
markworts2 <mark.worts@gmail.com>
2023-05-05 16:01:18 +0100

    try

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -6,15 +6,16 @@ from machine import Pin, SPI
 import max7219
 
 print('in publish max7219')
-print('version 20230505 v_0.01')
+print('version 20230505 v_0.02')
 
 
-
-display = max7219.SevenSegment(digits=16, scan_digits=8, cs=12, spi_bus=2, reverse=True)
-display.text("ABCDEF")
-display.number(3.14159)
-display.message("Hello World")
-display.clear()
+try:
+    display = max7219.SevenSegment(digits=16, scan_digits=8, cs=12, spi_bus=2, reverse=True)
+    display.text("ABCDEF")
+    display.number(3.14159)
+    display.message("Hello World")
+    display.clear()
+except Exception as e: print(str(e))
 
 
 sleep(100000)
