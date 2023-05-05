c0f9d6077cd64956bceb81626c6e726c70cd6aee
markworts2 <mark.worts@gmail.com>
2023-05-05 15:46:52 +0100

    print text

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -8,10 +8,13 @@ import max7219
 print('in publish max7219')
 print('version 20230505 v_0.01')
 
+
 spi = SPI(1, baudrate=10000000)
 screen = max7219.Max7219(64, 8, spi, Pin(12))
 screen.text('12345678', 0, 0, 1)
 screen.show()
+print('Should be 123445678')
+
 
 sleep(100000)
 print('end sleep')
