5fcc54ff377105da510050c0be3c68ade6f2e902
markworts2 <mark.worts@gmail.com>
2023-05-05 14:54:45 +0100

    message and sleep

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -5,7 +5,13 @@ import machine
 from machine import Pin, SPI
 import max7219
 
+print('in publish max7219')
+print('version 20230505 v_0.01')
+
 spi = SPI(1, baudrate=10000000)
 screen = max7219.Max7219(64, 8, spi, Pin(15))
 screen.text('ABCDEFGH', 0, 0, 1)
 screen.show()
+
+sleep(100000)
+print('end sleep')
