c323794ad20f9f87f6f34e794a4ddeb7591dbf04
markworts2 <mark.worts@gmail.com>
2023-05-05 17:02:04 +0100

    bus 1

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -12,7 +12,7 @@ print('version 20230505 v_0.02')
 
 
 try:
-    display = max7219.SevenSegment(digits=8, scan_digits=8, cs=15, spi_bus=2, reverse=True)
+    display = max7219.SevenSegment(digits=8, scan_digits=8, cs=15, spi_bus=1, reverse=True)
     print("ABCDEF")
     display.text("ABCDEF")
     sleep(100)
