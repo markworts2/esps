22d0ae76c9c404a03bd10c215ead059c871f4630
mark <mark.worts@gmail.com>
2023-07-21 08:18:16 +0100

    baseline

---
+++ water/publish_dht_esp32.py
@@ -0,0 +1,70 @@
+from time import sleep
+from time import time
+from umqtt.simple import MQTTClient
+import machine
+from dht import DHT22
+import onewire, ds18x20
+from struct import unpack
+from machine import Pin
+
+print ('dht_publish_water_esp32.py  v23/02/08 08:29')
+
+p4 = Pin(4, Pin.OUT) #power pin
+p5 = Pin(5, Pin.IN) #read water pin
+
+print('turn on power pin 4')
+p4.value(1) #turn on the power pin
+
+#connect to mqqt
+SERVER = '192.168.86.248'  # MQTT Server Address (Change to the IP address of your Pi)
+CLIENT_ID = 'ESP32_DHT22_Sensor'
+TOPIC = b'temp_humidity'
+rtc = machine.RTC()
+
+print('Connecting to MQTT')
+client = MQTTClient(CLIENT_ID, SERVER)
+try:
+   client.connect()   # Connect to MQTT broker
+except IndexError:
+   print ('index error: can not connect to MQQT')
+
+#read p5 the plan water sensor and push to MQQT
+try:
+        date_str = "{2:02d}/{1:02d}/{0:4d} {4:02d}:{5:02d}".format(*rtc.datetime())
+        msg = date_str +  "," + str('p5') + "," + str(p5.value())
+        client.publish(TOPIC, msg)
+        print (msg)
+except:
+        date_str = "{2:02d}/{1:02d}/{0:4d} {4:02d}:{5:02d}".format(*rtc.datetime())
+        msg = date_str +  "," + 'reading failed'
+        print('Failed to read sensor.')
+        client.publish(TOPIC,msg)
+
+sleep(5)
+print('turn off power pin')
+p4.value(0)
+
+ds_pin = machine.Pin(21)
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+
+roms = ds_sensor.scan()
+print('Found DS devices: ', roms)
+
+try:
+    ds_sensor.convert_temp()
+    sleep(1)
+    for rom in roms:
+      try:
+        date_str = "{2:02d}/{1:02d}/{0:4d} {4:02d}:{5:02d}".format(*rtc.datetime())
+        print ('rom :',rom)
+        msg = date_str +  "," + str(unpack('<H', rom)[0]) + "," + str(ds_sensor.read_temp(rom))
+        client.publish(TOPIC, msg)  # Publish sensor data to MQTT topic
+        print(msg)
+      except:
+        client.publish(TOPIC, '')
+except OSError:
+    d = time()
+    msg = (b'{}'.format(d))
+    print('Failed to read sensor.')
+    client.publish(TOPIC,msg)
+sleep(5)
