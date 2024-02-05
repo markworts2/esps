cf662bf529bd2e8513289b1237ff7343b54d6c9b
markworts2 <mark.worts@gmail.com>
2024-02-05 17:11:42 +0000

    spi_bus=2

--- publish_work_esp8266.py
+++ publish_work_esp8266.py
@@ -8,7 +8,7 @@ try:
 except Exception as e: print(str(e))
 
 print('in publish max7219')
-print('version 20240205 v_0.02')
+print('version 20240205 v_0.03')
 
 DIN = 13
 CS = 12
@@ -20,7 +20,7 @@ pclk = Pin(CLK, Pin.OUT)
 
 
 try:
-    display = max7219.SevenSegment(digits=8, scan_digits=4, cs=5, spi_bus=1, reverse=False)
+    display = max7219.SevenSegment(digits=8, scan_digits=4, cs=5, spi_bus=2, reverse=False)
     print("ABCDEF")
     display.text("ABCDEF")
     sleep(10)
